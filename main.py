4f47a5583c22ec9cb1881272d5932ce87dd316bb
stefbil <32976511+stefbil@users.noreply.github.com>
2024-05-13 19:31:44 +0300

    v1

---
+++ main.py
@@ -0,0 +1,51 @@
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from datasets import load_dataset
+import gradio as gr
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-large-v3"
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+).to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=30,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+dataset = load_dataset("distil-whisper/librispeech_long", "clean", split="validation")
+sample = dataset[0]["audio"]
+
+def speech_to_text(speech):
+    text= pipe(speech)["text"]
+    return text
+
+demo = gr.Blocks()
+
+with demo:
+    audio_file = gr.Audio(type="filepath")
+    text= gr.Textbox()
+
+    b1 = gr.Button("Convert to text!")
+
+    b1.click(speech_to_text, inputs=audio_file, outputs=text)
+
+demo.launch()
+
+
+
+# result = pipe("M:\Coding\speech2textenv\COSMOTE Grow Your Business -  Podcasts\Episode1.mp3")
+# print(result["text"])
